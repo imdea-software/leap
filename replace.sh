ef96c0e9a701db651d191a70ce457bb547497e7f
Alejandro Sanchez <alesanz@gmail.com>
2016-07-08 20:11:23 +0200

    Old files removed.

--- replace.sh
+++
@@ -1,5 +0,0 @@
-for i in `find src/* -type f | grep -v \\.swp | grep -v \\.svn` ; do
-	sed -i "" 's/'$1'/'$2'/g' $i ; done
-
-#	LinuxVersion sed -i 's/'$1'/'$2'/g' $i ; done
-#	MacVersion sed -i "" 's/'$1'/'$2'/g' $i ; done
